1f8649cbb80af61f63bf7455a6e1b1ec05e6c114
Jeongbowoon <happyanne200212@gmail/com>
2024-05-24 19:15:03 +0900

    feat: 회원가입 구현

--- back-end/src/main/java/com/se/demo/dto/MemberDTOOfBW.java
+++ back-end/src/main/java/com/se/demo/dto/MemberDTOOfBW.java
@@ -1,4 +1,21 @@
 package com.se.demo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class MemberDTOOfBW {
+    //view와 소통
+
+ private int id;
+
+ private String nickName;
+
+ private String password;
+
 }
+
+/*
+class AdminDTOOfBW extends MemberDTOOfBW{
+}*/
