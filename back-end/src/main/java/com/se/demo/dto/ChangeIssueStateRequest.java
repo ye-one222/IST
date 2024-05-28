d92c6d1b6aa74c3f4656122ad493e20f94862952
Jeongbowoon <happyanne200212@gmail/com>
2024-05-29 01:46:32 +0900

    feat: state change assigned, fixed

---
+++ back-end/src/main/java/com/se/demo/dto/ChangeIssueStateRequest.java
@@ -0,0 +1,12 @@
+package com.se.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class ChangeIssueStateRequest {
+    private Integer assignee_id;
+    private String state;
+}
